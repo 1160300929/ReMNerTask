e39149e40ebec383f30d892433111e60bc557b06
zhengzihao <1750613426@qq.com>
2020-11-11 11:17:57 +0800

    进行train函数的编写

--- run_ner.py
+++ run_ner.py
@@ -47,7 +47,213 @@ except ImportError:
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
 logger = logging.getLogger(__name__)
 
-def train(args, train_dataset, model,encoder,tokenizer, labels, pad_token_label_id):
+def train_Object(args, train_dataset, model,encoder,encoder_cfg,tokenizer, labels, pad_token_label_id):
+    if args.local_rank in [-1, 0]:
+        tb_writer = SummaryWriter(args.output_dir)
+
+    args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
+    train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset,
+                                  sampler=train_sampler,
+                                  batch_size=args.train_batch_size,
+                                  )
+
+    if args.max_steps > 0:
+        t_total = args.max_steps
+        args.num_train_epochs = args.max_steps // (len(train_dataloader) // args.gradient_accumulation_steps) + 1
+    else:
+        t_total = len(train_dataloader) // args.gradient_accumulation_steps * args.num_train_epochs
+
+    args.logging_steps = eval(args.logging_steps)
+    if isinstance(args.logging_steps, float):
+        args.logging_steps = int(args.logging_steps * len(train_dataloader)) // args.gradient_accumulation_steps
+
+    args.bert_lr = args.bert_lr if args.bert_lr else args.learning_rate
+    args.classifier_lr = args.classifier_lr if args.classifier_lr else args.learning_rate
+    args.crf_lr = args.crf_lr if args.crf_lr else args.learning_rate
+    optimizer = AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
+    )
+
+    # Check if saved optimizer or scheduler states exist
+    if os.path.isfile(os.path.join(args.model_name_or_path, "optimizer.pt")) and os.path.isfile(
+            os.path.join(args.model_name_or_path, "scheduler.pt")
+    ):
+        # Load in optimizer and scheduler states
+        optimizer.load_state_dict(torch.load(os.path.join(args.model_name_or_path, "optimizer.pt")))
+        scheduler.load_state_dict(torch.load(os.path.join(args.model_name_or_path, "scheduler.pt")))
+
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
+
+    # multi-gpu training (should be after apex fp16 initialization)
+    if args.n_gpu > 1:
+        model = torch.nn.DataParallel(model)
+
+    # Distributed training (should be after apex fp16 initialization)
+    if args.local_rank != -1:
+        model = torch.nn.parallel.DistributedDataParallel(
+            model, device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=True
+        )
+    # Train!
+    logger.info("***** Running training *****")
+    logger.info("  Num examples = %d", len(train_dataset))
+    logger.info("  Num Epochs = %d", args.num_train_epochs)
+    logger.info("  Instantaneous batch size per GPU = %d", args.per_gpu_train_batch_size)
+    logger.info(
+        "  Total train batch size (w. parallel, distributed & accumulation) = %d",
+        args.train_batch_size
+        * args.gradient_accumulation_steps
+        * (torch.distributed.get_world_size() if args.local_rank != -1 else 1),
+    )
+    logger.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
+    logger.info("  Total optimization steps = %d", t_total)
+    #whether to frozen the visual encoder
+    if args.frozen_encoder is True:
+        for param in encoder.base_model.parameters():
+            param.requires_grad = False
+
+    global_step = 0
+    epochs_trained = 0
+    best_score = 0.0
+    steps_trained_in_current_epoch = 0
+    # Check if continuing training from a checkpoint
+    if os.path.exists(args.model_name_or_path):
+        # set global_step to gobal_step of last saved checkpoint from model path
+        try:
+            global_step = int(args.model_name_or_path.split("-")[-1].split("/")[0])
+        except ValueError:
+            global_step = 0
+        epochs_trained = global_step // (len(train_dataloader) // args.gradient_accumulation_steps)
+        steps_trained_in_current_epoch = global_step % (len(train_dataloader) // args.gradient_accumulation_steps)
+
+        logger.info("  Continuing training from checkpoint, will skip to saved global_step")
+        logger.info("  Continuing training from epoch %d", epochs_trained)
+        logger.info("  Continuing training from global step %d", global_step)
+        logger.info("  Will skip the first %d steps in the first epoch", steps_trained_in_current_epoch)
+
+    tr_loss, logging_loss = 0.0, 0.0
+    model.zero_grad()
+    train_iterator = trange(
+        epochs_trained, int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0]
+    )
+    set_seed(args)  # Added here for reproductibility
+    for _ in train_iterator:
+        epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
+        for step, batch in enumerate(epoch_iterator):
+
+            # Skip past any already trained steps if resuming training
+            if steps_trained_in_current_epoch > 0:
+                steps_trained_in_current_epoch -= 1
+                continue
+
+            model.train()
+            batch = tuple(t.to(args.device) for t in batch)
+            inputs = {
+                "input_ids": batch[0],
+                "input_mask": batch[1],
+                "added_input_mask": batch[2],
+                "segment_ids": batch[3],
+                "image": batch[4],
+                "sizes": batch[5],
+                "scales_yx": batch[6],
+                "label_id": batch[7]
+            }
+            #todo 将图片变成输入的特征
+            output_dict = encoder(
+                inputs['images'],
+                inputs['sizes'],
+                inputs['scales_yx'],
+                padding="max_detections",
+                max_detections=encoder_cfg.max_detections,
+                return_tensors='pt'
+            )
+
+            #todo:将inputs组织成模型可以接受的输入
+
+            outputs = model(**inputs)
+            if args.model_type != "distilbert":
+                inputs["token_type_ids"] = (
+                    batch[3] if args.model_type in ["bert", "xlnet"] else None
+                )  # XLM and RoBERTa don"t use segment_ids
+
+            outputs = model(**inputs)
+            loss = outputs[0]  # model outputs are always tuple in pytorch-transformers (see doc)
+
+            if args.n_gpu > 1:
+                loss = loss.mean()  # mean() to average on multi-gpu parallel training
+            if args.gradient_accumulation_steps > 1:
+                loss = loss / args.gradient_accumulation_steps
+
+            if args.fp16:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+
+            tr_loss += loss.item()
+            epoch_iterator.set_description('Loss: {}'.format(round(loss.item(), 6)))
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                if args.fp16:
+                    torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args.max_grad_norm)
+                else:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+
+                optimizer.step()
+                scheduler.step()  # Update learning rate schedule
+                model.zero_grad()
+                global_step += 1
+
+                if args.local_rank in [-1, 0] and args.logging_steps > 0 and global_step % args.logging_steps == 0:
+                    # Log metrics
+                    if (
+                            args.local_rank == -1 and args.evaluate_during_training
+                    ):  # Only evaluate when single GPU otherwise metrics may not average well
+                        results, _ = evaluate(args, model, tokenizer, labels, pad_token_label_id, mode="dev",
+                                              prefix=global_step)
+                        for key, value in results.items():
+                            if isinstance(value, float) or isinstance(value, int):
+                                tb_writer.add_scalar("eval_{}".format(key), value, global_step)
+                    tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
+                    tb_writer.add_scalar("loss", (tr_loss - logging_loss) / args.logging_steps, global_step)
+                    logging_loss = tr_loss
+
+                    if best_score < results['f1']:
+                        best_score = results['f1']
+                        output_dir = os.path.join(args.output_dir, "best_checkpoint")
+                        if not os.path.exists(output_dir):
+                            os.makedirs(output_dir)
+                        model_to_save = (
+                            model.module if hasattr(model, "module") else model
+                        )  # Take care of distributed/parallel training
+                        model_to_save.save_pretrained(output_dir)
+                        tokenizer.save_pretrained(output_dir)
+
+                        torch.save(args, os.path.join(output_dir, "training_args.bin"))
+                        logger.info("Saving model checkpoint to %s", output_dir)
+
+                        torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
+                        torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
+                        logger.info("Saving optimizer and scheduler states to %s", output_dir)
+
+            if args.max_steps > 0 and global_step > args.max_steps:
+                epoch_iterator.close()
+                break
+        if args.max_steps > 0 and global_step > args.max_steps:
+            train_iterator.close()
+            break
+
+    if args.local_rank in [-1, 0]:
+        tb_writer.close()
+
+    return global_step, tr_loss / global_step
+
+def train_Grid(args, train_dataset, model,encoder,tokenizer, labels, pad_token_label_id):
     if args.local_rank in [-1, 0]:
         tb_writer = SummaryWriter(args.output_dir)
 
@@ -158,7 +364,13 @@ def train(args, train_dataset, model,encoder,tokenizer, labels, pad_token_label_
                       "attention_mask": batch[1],
                       "valid_mask": batch[2],
                       "labels": batch[4],
-                      "img_feature":batch[5]}
+                      "img_feature":batch[5]
+
+
+
+
+
+                      }
             if args.model_type != "distilbert":
                 inputs["token_type_ids"] = (
                     batch[3] if args.model_type in ["bert", "xlnet"] else None
@@ -234,8 +446,6 @@ def train(args, train_dataset, model,encoder,tokenizer, labels, pad_token_label_
         tb_writer.close()
 
     return global_step, tr_loss / global_step
-
-
 
 
 
